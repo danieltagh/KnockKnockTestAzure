c92592a300736c46f30187b7beb9ef732a140a62
daniel.taghaddos@gmail.com <danial.taghaddos@gmail.com>
2019-01-04 09:08:27 +0000

    Token API added

---
+++ NewAzureTestWebApp/Controllers/TokenController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NewAzureTestWebApp.Controllers
+{
+    public class TokenController : ApiController
+    {
+        [HttpGet]
+        public object Index()
+        {
+            return "9f33ef6f-9a39-40e5-9a7b-80cf78f5405e";
+        }
+    }
+}
